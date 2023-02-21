b5033cbad184c70127dd86c3fc1d45c6b14203b5
MayuRuru <aruizrull@protonmail.com>
2023-02-21 08:57:53 +0100

    Add cookie parser

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3500;
+const { logger } = require("./middleware/logger");
+const { errorHandler } = require("./middleware/errorHandler");
+
+// Our custom middleware imported above:
+app.use(logger);
 
 // Middleware for json:
 app.use(express.json());
+
+// Parse cookies we receive:
+app.use(cookieParser());
 
 // Static middleware:
 app.use("/", express.static(path.join(__dirname, "public")));
@@ -22,5 +31,7 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not Found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
